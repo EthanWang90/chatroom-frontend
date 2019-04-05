abf21e9cf4d4545186ce84522b44f315a15a4838
ethan <ethanwang9011@gmail.com>
2019-04-06 08:50:27 +1300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Row, Col } from 'antd';
+import Button from 'antd/lib/button';
+import Iframe from './components/Iframe'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Row>
+          <Col xs={{ span: 22, offset: 1 }} sm={{ span: 8, offset: 8 }}>
+            <Iframe></Iframe>
+          </Col>
+        </Row>
+        <Row>
+          <Button type="primary">Button</Button>
+        </Row>
       </div>
     );
   }
---
+++ src/components/Iframe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Iframe extends React.Component{
+
+    render(){
+        return(
+            <div className="scrollable-container">
+                <div className="background"></div>
+            </div>
+        )
+    }
+}
